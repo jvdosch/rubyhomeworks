3ff8f281f5b32afa85d1dda17a56613deaf6ed88
Jon Dosch <jdosch27@gmail.com>
2012-10-11 20:09:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facecook::Application.config.secret_token = 'de9fd6f60a2b71e184fba45b2ba8af20b90661ab27b4bbbeea56c913fa99ddeec71159884b598b5f1c3c4b3fafcb9d0fd793f4b1bde55c5aca096c581e401e36'
